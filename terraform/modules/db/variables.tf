42ba024faa77aa76ca544cba59c2d4cea7a34082
eogladkih <eogladkih@hotmail.com>
2018-07-09 16:34:15 +0300

    Terraform-2 main HW

--- terraform/modules/db/variables.tf
+++ terraform/modules/db/variables.tf
@@ -1,12 +1,13 @@
-# variable "zone" {
-#   description = "zone"
-#   default     = "europe-west1-b"
-# }
-# variable "db_disk_image" {
-#   description = "Disck image for reddit db"
-#   default     = "reddit-db-base"
-# }
-# variable "public_key_path" {
-#   description = "Path to the public key used for ssh access"
-# }
+variable "zone" {
+  description = "zone"
+  default     = "europe-west1-b"
+}
 
+variable "db_disk_image" {
+  description = "Disck image for reddit db"
+  default     = "reddit-db-base"
+}
+
+variable "public_key_path" {
+  description = "Path to the public key used for ssh access"
+}
